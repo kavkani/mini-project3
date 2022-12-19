40df8675749fc7e3fc280837448f96ea3e0e0c63
amirmohammad <kavkaniamirmohammad@gmail.com>
2022-12-20 00:40:37 +0330

    african ref def!

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import matplotlib.pyplot as plt
+def african_ref():
+    pmd = open('referees.csv', encoding='utf-8')
+    pmd1 = pmd.readlines()
+    return pmd1
+
+
+lst1 = african_ref()
+africn_ref_lst = []
+for i in lst1:
+    lst1.append(i[:-1].split(','))
+
+print(lst1)
